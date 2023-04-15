a98c9ea4ea61e3e9d16e1634135a5fcac71afc8c
Yaya LY <yaya.ly@uadb.edu.sn>
2023-04-15 02:27:50 +0000

    security configuration done

---
+++ src/main/java/com/yahyalbarmaki/security/config/SecurityFilterChain.java
@@ -0,0 +1,40 @@
+package com.yahyalbarmaki.security.config;
+
+import jakarta.servlet.Filter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.DefaultSecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityFilterChain {
+
+    private final JwtAuthenticationFilter jwtAuthFilter;
+    private final AuthenticationProvider authenticationProvider;
+
+    @Bean
+    public DefaultSecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity
+                .csrf()
+                .disable()
+                .authorizeHttpRequests()
+                .requestMatchers(" ")
+                .permitAll()
+                .anyRequest()
+                .authenticated()
+                .and()
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .authenticationProvider(authenticationProvider)
+                .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
+        return httpSecurity.build();
+    }
+}
---
+++ src/main/java/com/yahyalbarmaki/security/user/Role.java
@@ -0,0 +1,5 @@
+package com.yahyalbarmaki.security.user;
+
+public enum Role {
+    ROLE,ADMIN
+}
